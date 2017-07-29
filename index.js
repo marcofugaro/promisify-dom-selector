b3c566f42c3eb2996669ca861c8fe853c2f51b09
Marco Fugaro <marco.fugaro@gmail.com>
2017-07-29 20:00:59 +0200

    ✨  Add support for nodelist selectors

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+function isArrayLike(item) {
+  return Array.isArray(item) || item instanceof NodeList || item instanceof HTMLCollection
+}
+
 function promisifyDOMSelector(selector, context = document) {
   return (queryString) => {
     return new Promise((resolve) => {
       // check if there is already the element, if yes resolve and return
       const targetNode = selector.call(context, queryString)
-      if (targetNode) { // TODO  support querySelectorAll
+      if (targetNode || (isArrayLike(targetNode) && targetNode.length > 0)) {
         return resolve(targetNode)
       }
 
       // else watch for the dom element changes
       const attemptSelection = new MutationObserver((mutations, observer) => {
         const targetNode = selector.call(context, queryString)
-        if (!targetNode)
+        if (!targetNode || (isArrayLike(targetNode) && targetNode.length === 0))
           return
 
         observer.disconnect()
