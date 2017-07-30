3ae6c595002b44d091127e515b7efd7833c94f92
Marco Fugaro <marco.fugaro@gmail.com>
2017-07-30 10:24:22 +0200

    ⏩  Add es module output

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,27 @@
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify'
+import { minify } from 'uglify-es'
 import pkg from './package.json'
 
 export default {
   entry: 'src/index.js',
-  format: 'umd',
-  moduleName: 'promisifyDOMSelector',
   plugins: [
     resolve(),
     babel({
       exclude: 'node_modules/**',
     }),
-    uglify(),
+    uglify({}, minify)
   ],
-  dest: pkg.main,
+  targets: [
+		{
+			format: 'umd',
+			moduleName: 'promisifyDOMSelector',
+			dest: pkg.main,
+		},
+		{
+			format: 'es',
+			dest: pkg.module,
+		}
+	]
 }
