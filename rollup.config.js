7edb699cdb63de1a29b486e34ee043df47a4a093
Marco Fugaro <marco.fugaro@gmail.com>
2017-07-31 17:30:43 +0200

    📦  Add couple of useful rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify'
+import bundleSize from 'rollup-plugin-bundle-size'
 import pkg from './package.json'
 
 export default {
@@ -9,10 +11,12 @@ export default {
   moduleName: 'promisifyDOMSelector',
   plugins: [
     resolve(),
+    commonjs(),
     babel({
       exclude: 'node_modules/**',
     }),
     uglify(),
+    bundleSize(),
   ],
   dest: pkg.main,
 }
